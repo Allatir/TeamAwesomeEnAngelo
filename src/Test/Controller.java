002e62d36bc922a5c630e9060ddc8a75807d1a69
bradley velghe <bradley.velghe@student.vives.be>
2020-02-05 10:58:12 +0100

    Initial commit

---
+++ src/Test/Controller.java
@@ -0,0 +1,14 @@
+package Test;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class Controller {
+    public TextField txtGetal1;
+    public TextField txtGetal2;
+    public Label lblUitvoer;
+
+    public void Clicked_Execute(MouseEvent mouseEvent) {
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,7 @@
+module TeamAwesomeEnAngelo {
+requires javafx.fxml;
+requires javafx.controls;
+requires java.desktop;
+
+opens test ;
+}
