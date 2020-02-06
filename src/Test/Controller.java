cd6087b9d4996020b55cdb8b41a005fab167fa92
bradley velghe <bradley.velghe@student.vives.be>
2020-02-06 16:53:43 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/Test/Controller.java
+++ src/Test/Controller.java
@@ -19,9 +19,29 @@ public class Controller {
         lblUitvoer.setText(String.valueOf(Uitvoer));
     }
 
+<<<<<<< Updated upstream
     public void Clicked_Divide(MouseEvent mouseEvent) {
         final double dTeller = Double.parseDouble(txtGetal1.getText());
         final double dNoemer = Double.parseDouble(txtGetal2.getText());
+=======
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+
+=======
+=======
+>>>>>>> Siebe
+>>>>>>> Stashed changes
+    public void KlikPlus(MouseEvent mouseEvent) {
+        //test
+        double dGetal1 = Double.parseDouble(txtGetal1.getText());
+        double dGetal2 = Double.parseDouble(txtGetal2.getText());
+        double dSom = dGetal1 + dGetal2;
+        lblUitvoer.setText(String.valueOf(dSom));
+        System.out.println("test");
+=======
+>>>>>>> Stashed changes
 
         double dResult = dTeller / dNoemer;
         lblUitvoer.setText("" + dResult);
@@ -29,4 +49,8 @@ public class Controller {
         //test
 >>>>>>> Angelo_Test
     }
+<<<<<<< HEAD
+>>>>>>> Angelo
+=======
+>>>>>>> Siebe
 }
